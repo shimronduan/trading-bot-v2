f28a5f5a125996ab55fcf3a2d27cc437931d7d70
shimronduan <shimron_duan@next.co.uk>
2025-07-23 00:32:21 +0100

    Add imports for Azure Table Storage and trading configuration in function app

--- function_app.py
+++ function_app.py
@@ -1,5 +1,13 @@
 import azure.functions as func
 import logging
+from decimal import Decimal, getcontext
+import json
+import os
+from azure_table_storage import AzureTableStorage
+from trading_config import SYMBOL
+import base64
+# from futures_client import FuturesClient
+# from azure.storage.queue import QueueClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
