25ae50a08b6ba6d045abb78dea7b3c71b05dab9b
shimronduan <shimron_duan@next.co.uk>
2025-07-22 23:27:59 +0100

    Add storage queue and table resources; update function app settings with storage connection string

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,25 @@ resource "azurerm_storage_account" "main" {
   account_replication_type = "LRS"
 }
 
+# 2. Create the storage account required by the function app
+resource "azurerm_storage_account" "botstorage" {
+  name                     = "tradingbotv2sa" # Must be globally unique
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_queue" "orders" {
+  name                 = "orders"
+  storage_account_name = azurerm_storage_account.botstorage.name
+}
+
+resource "azurerm_storage_table" "takeprofitandstoploss" {
+  name                 = "TakeProfitAndStopLoss"
+  storage_account_name = azurerm_storage_account.botstorage.name
+}
+
 # 3. Create the Application Insights for monitoring
 # resource "azurerm_application_insights" "main" {
 #   name                = "trading-bot-app-v2-insights"
@@ -70,6 +89,7 @@ resource "azurerm_linux_function_app" "main" {
 
   app_settings = {
     # "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.main.instrumentation_key
-    "FUNCTIONS_EXTENSION_VERSION"    = "~4"
+    "FUNCTIONS_EXTENSION_VERSION"           = "~4"
+    "AZURE_STORAGE_CONNECTION_STRING"       = azurerm_storage_account.botstorage.primary_connection_string
   }
 }
