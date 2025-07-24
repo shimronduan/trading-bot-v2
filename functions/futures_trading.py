97c8f3c0ee893dd37fb50c597ac1ccdc13f1a6e2
shimronduan <shimron_duan@next.co.uk>
2025-07-24 17:47:18 +0100

    Refactor futures trading logic to include ATR in trade execution and improve request handling

--- functions/futures_trading.py
+++ functions/futures_trading.py
@@ -12,7 +12,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         raw_body = req.get_body()
         logging.info(f"Raw request body: {raw_body}")
-        signal_type = raw_body.decode('utf-8').title().strip()
+
+        body = json.loads(raw_body)
+        signal_type = body.get("type", "").title().strip()
+        atr = float(body.get("atr", "0.0"))
+        # signal_type = raw_body.decode('utf-8').title().strip()
 
         response_message = ""
         if signal_type == "Close":
@@ -26,7 +30,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
             quantity = client.calculate_trade_quantity()
             records = ats_client.list_records()
-            response_message = client.execute_trade_with_sl_tp(desired_side, quantity, records)
+            response_message = client.execute_trade_with_sl_tp(desired_side, quantity, records, atr)
 
             try:
                 queue_client = create_queue_client(queue_name="orders")
--- functions/testing.py
+++ functions/testing.py
@@ -4,8 +4,12 @@ from trading_config import SYMBOL
 from utils.client_factory import create_futures_client
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    client = create_futures_client()
-    client.close_position_if_no_open_orders(SYMBOL)
-    client.cancel_orders_if_no_position(SYMBOL)
+    raw_body = req.get_body()
+    body = json.loads(raw_body)
+    type = body.get("type", "").title().strip()
+    atr = float(body.get("atr", "0.0"))
+    # client = create_futures_client()
+    # client.close_position_if_no_open_orders(SYMBOL)
+    # client.cancel_orders_if_no_position(SYMBOL)
 
     return func.HttpResponse(json.dumps({"status": "success", "message": "Message DOGEUSDT enqueued"}), status_code=200, mimetype="application/json")
