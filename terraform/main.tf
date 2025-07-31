d1a9e3971e99016bf8c6a12dd808723abbeb5b3f
shimronduan <shimron_duan@next.co.uk>
2025-08-01 00:39:34 +0100

    Restore role assignment resource for storage access in function app

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,16 +79,16 @@
   }
 
 
-  # resource "azurerm_role_assignment" "storage_roleassignment" {
-  #   scope = azurerm_storage_account.storageAccount.id
-  #   role_definition_name = "Storage Blob Data Owner"
-  #   principal_id = azurerm_function_app_flex_consumption.functionApps.identity[0].principal_id
-  #   principal_type = "ServicePrincipal"
-  #   timeouts {
-  #     create = "60m"
-  #   }
-  #   depends_on = [azurerm_function_app_flex_consumption.functionApps]
-  # }
+  resource "azurerm_role_assignment" "storage_roleassignment" {
+    scope = azurerm_storage_account.storageAccount.id
+    role_definition_name = "Storage Blob Data Owner"
+    principal_id = azurerm_function_app_flex_consumption.functionApps.identity[0].principal_id
+    principal_type = "ServicePrincipal"
+    timeouts {
+      create = "60m"
+    }
+    depends_on = [azurerm_function_app_flex_consumption.functionApps]
+  }
 
 
   # # 5. Create the Linux Function App
