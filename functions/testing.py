b1861d649627f1a9fe8e18d73904dbe29643b97a
shimronduan <shimron_duan@next.co.uk>
2025-08-01 00:55:30 +0100

    Remove unused client operations from testing function

--- functions/testing.py
+++ functions/testing.py
@@ -1,11 +1,10 @@
+import logging
 import azure.functions as func
 import json
 from trading_config import SYMBOL
 from utils.client_factory import create_futures_client
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    client = create_futures_client()
-    client.close_position_if_no_open_orders(SYMBOL)
-    client.cancel_orders_if_no_position(SYMBOL)
+    logging.info('Testing function triggered')
 
     return func.HttpResponse(json.dumps({"status": "success", "message": "Message DOGEUSDT enqueued"}), status_code=200, mimetype="application/json")
