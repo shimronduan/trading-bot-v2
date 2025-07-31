fb4106b0aa6c43caf196ddf31736e97345359c1c
shimronduan <shimron_duan@next.co.uk>
2025-07-31 13:29:11 +0100

    Fix resource group references in storage account configuration for consistency

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "botstorage" {
   name                     = "tradingbotv2sa" # Must be globally unique
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
