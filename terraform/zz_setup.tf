9c27e96e7be870bc55f630ae5978cc93db1a74cd
shimronduan <shimron_duan@next.co.uk>
2025-08-12 10:07:43 +0100

    Revert "Update app version to v2 and adjust Terraform configurations for Singapore deployment"
    This reverts commit 3f0599c1f5443d9e23b17adb55357d811403540d.

--- terraform/zz_setup.tf
+++ terraform/zz_setup.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstatesa17329"
     container_name       = "trading-bot-v2-tfstate"
-    key                  = "prod-Singapore.terraform.tfstate"
+    key                  = "prod.terraform.tfstate"
   }
 }
 # Configure the Azure Provider
